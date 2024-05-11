213f4ed3ab506e32429d690f4b7c14a7dbca673a
Dennis Ruppel <crap-dennis@gmx.net>
2024-05-11 05:31:44 +0200

    Add validation layer

--- src/vulkan_backend/mod.rs
+++ src/vulkan_backend/mod.rs
@@ -1,10 +1,16 @@
 use std::{collections::HashSet, sync::Arc};
 
 use anyhow::{anyhow, Result};
-use log::info;
+use log::{debug, error, info, trace, warn};
 
 use vulkano::{
-    instance::{Instance, InstanceCreateInfo, InstanceExtensions},
+    instance::{
+        debug::{
+            DebugUtilsMessageSeverity, DebugUtilsMessenger, DebugUtilsMessengerCallback,
+            DebugUtilsMessengerCreateInfo,
+        },
+        Instance, InstanceCreateInfo, InstanceExtensions,
+    },
     VulkanLibrary,
 };
 
@@ -12,6 +18,7 @@ const VALIDATION_LAYER: &str = "VK_LAYER_KHRONOS_validation";
 
 pub struct Vulkan {
     instance: Arc<Instance>,
+    debug_messenger: DebugUtilsMessenger,
 }
 
 impl Vulkan {
@@ -21,7 +28,7 @@ impl Vulkan {
 
         library
             .layer_properties()?
-            .for_each(|l| info!("Got layer: {}", l.name()));
+            .for_each(|l| info!("Found layer: {}", l.name()));
 
         let layer_set = library
             .layer_properties()?
@@ -35,12 +42,13 @@ impl Vulkan {
 
         let layers = if debug {
             vec![String::from(VALIDATION_LAYER)]
+            // vec![]
         } else {
             vec![]
         };
 
         let extensions = InstanceExtensions {
-            ext_debug_utils: true,
+            ext_debug_utils: debug,
             // Can not currently automatically figure out the required extensions because this
             // requires a release after Vulkano 0.34 to be compatible with winit 0.30.
             // ..Surface::required_extensions(&event_loop)
@@ -49,14 +57,53 @@ impl Vulkan {
             ..Default::default()
         };
 
+        let mut debug_info;
+        unsafe {
+            let callback =
+                DebugUtilsMessengerCallback::new(|severity, message_type, callback_data| {
+                    let message = callback_data.message;
+
+                    match severity {
+                        DebugUtilsMessageSeverity::ERROR => {
+                            error!("({:?}) {}", message_type, message)
+                        }
+                        DebugUtilsMessageSeverity::WARNING => {
+                            warn!("({:?}) {}", message_type, message)
+                        }
+                        DebugUtilsMessageSeverity::INFO => {
+                            info!("({:?}) {}", message_type, message)
+                        }
+                        DebugUtilsMessageSeverity::VERBOSE => {
+                            trace!("({:?}) {}", message_type, message)
+                        }
+                        _ => error!("Unexpected severity"),
+                    }
+                });
+
+            // Make a callback for all severity levels. By default this excludes verbose and info.
+            debug_info = DebugUtilsMessengerCreateInfo::user_callback(callback);
+            debug_info.message_severity = DebugUtilsMessageSeverity::VERBOSE
+                | DebugUtilsMessageSeverity::INFO
+                | DebugUtilsMessageSeverity::WARNING
+                | DebugUtilsMessageSeverity::ERROR;
+        }
+
         let info = InstanceCreateInfo {
             enabled_extensions: extensions,
             enabled_layers: layers,
+            // This does not work in Vulkano 0.34 but is already fixed in main branch.
+            // debug_utils_messengers: vec![debug_info.clone()],
             ..InstanceCreateInfo::application_from_cargo_toml()
         };
 
         let instance = Instance::new(library, info)?;
 
-        Ok(Self { instance })
+        // Keep the messenger alive until the end of the program or the callback will stop working.
+        let debug_messenger = DebugUtilsMessenger::new(instance.clone(), debug_info)?;
+
+        Ok(Self {
+            instance,
+            debug_messenger,
+        })
     }
 }
